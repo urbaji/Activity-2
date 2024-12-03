64de6a88bead3bc2434f8fb5da1298d1cc45e23b
urbaji <145362879+urbaji@users.noreply.github.com>
2024-12-03 19:07:35 +0500

    Add files via upload

---
+++ Chekcing if number is positvie or not-activity-2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter your number:"))
+
+if num1 > 0:
+    print(num1 ," is a positive number.")
+elif num1 == 0: 
+    print(num1 ," is neither postive nor negative.")
+else:
+    print(num1 ," is a negative number")
